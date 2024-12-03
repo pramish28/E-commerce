ce42550bde2c826971e29fff7a24d5e9782ee288
pramish28 <pramishsharma78@gmail.com>
2024-12-03 22:22:15 +0545

    Displaying and Deleting Cart Item

--- models/product.js
+++ models/product.js
@@ -51,6 +51,9 @@ module.exports = class Product {
      static deleteById(id) {
       getProductsFromFile(products => {
        const product = products.find(prod => prod.id === id);
+       if (!product) {
+        return;
+       }
         const updatedProducts = products.filter(prod => prod.id !== id) ;
       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
         if(!err){
