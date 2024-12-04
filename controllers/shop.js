55f702c0710b51e77710fb76691e0bb96d43eb13
pramish28 <pramishsharma78@gmail.com>
2024-12-04 15:45:12 +0545

    Inserting our data into data base

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll() 
+  .then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  }) 
+  .catch(err => console.log(err)); 
 };
-
+ 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
