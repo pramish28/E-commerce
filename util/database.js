09a92695442ca5f9d482000b738fdec845738734
pramish28 <pramishsharma78@gmail.com>
2025-02-02 19:45:52 +0545

    Installing Mongodb in the project

--- util/database.js
+++ util/database.js
@@ -1,9 +1,19 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete','root','sharma',{
- dialect: 'mysql',
- host: 'localhost'
+const mongoConnect =(callback) => {
+
+MongoClient.connect(
+    'mongodb+srv://pramish:password%40psp123@cluster0.0g070.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'
+)
+.then(client => {
+  console.log('Connected!');
+    callback(client);
+})
+.catch(err => {
+  console.log(err);
+ // throw err;
 });
+};
 
- module.exports = sequelize;
-
+module.exports = mongoConnect;
