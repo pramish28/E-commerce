e457906aa1ab5907004872a604761c0032c335d3
pramish28 <pramishsharma78@gmail.com>
2025-02-02 23:08:00 +0545

    fetching all the products and made working of the edit and the delete button using mongodb

--- models/product.js
+++ models/product.js
@@ -1,17 +1,29 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl , id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
    save() {
-    const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
-      .then(result => {
+     const db = getDb();
+     let dbOp;
+     if (this._id) {
+      //update the product
+      dbOp = db
+      .collection('products')
+      .updateOne({_id: this._id }, { $set: this});
+     } else {
+       dbOp = db
+       .collection('products')
+        .insertOne(this); 
+     }
+     return dbOp
+        .then(result => {
         console.log(result);
       })
       .catch(err => {
@@ -31,7 +43,25 @@ class Product {
       .catch(err => {
         console.log(err);
       });
-} 
+ } 
+    static findByPk(prodId) {
+    const db = getDb();
+    return db
+    .collection('products')
+    .find({ _id:new mongodb.ObjectId(prodId)})
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    }
+    
+
+
+
 }
 
 
