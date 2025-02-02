4b1f367e36d78d7c064a4d3841d7b29f6a02ba74
pramish28 <pramishsharma78@gmail.com>
2025-02-02 21:29:28 +0545

    Fetching All Products

--- util/database.js
+++ util/database.js
@@ -1,19 +1,30 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect =(callback) => {
 
 MongoClient.connect(
-    'mongodb+srv://pramish:password%40psp123@cluster0.0g070.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://pramish:password%40psp123@cluster0.0g070.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
 )
 .then(client => {
   console.log('Connected!');
+  _db = client.db();
     callback(client);
 })
 .catch(err => {
   console.log(err);
- // throw err;
+  throw err;
 });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
