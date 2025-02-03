07c529b1e466482c062923062e6a4585b27b0dcd
pramish28 <pramishsharma78@gmail.com>
2025-02-03 16:55:12 +0545

    Making the delete option working

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,23 +19,26 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
- User.findByPk('679fb46396b91ff71a7ba43a')
-   .then(user => {
-     req.user = new User(user.name, user.email, user.cart, user._id);
-     next();
-   })
-   .catch(err => console.log(err));
+// app.use((req, res, next) => {
+// User.findByPk('679fb46396b91ff71a7ba43a')
+//    .then(user => {
+//      req.user = new User(user.name, user.email, user.cart, user._id);
+//      next();
+//    })
+//    .catch(err => console.log(err));
  
-});
+// });
 
  app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
  
-mongoConnect(() => {
-
-app.listen(3000);
-});
+mongoose.connect('mongodb+srv://pramish:password%40psp123@cluster0.0g070.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+.then(
+  result => {
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  });
  
